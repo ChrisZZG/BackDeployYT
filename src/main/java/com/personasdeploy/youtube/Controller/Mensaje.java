af3e91cdefb5d192b602c2cfa6c67e43b88a34b3
ChrisZZG <cristiangonzz@gmail.com>
2023-01-13 06:27:42 -0300

    Primera Version Back para Deploy

---
+++ src/main/java/com/personasdeploy/youtube/Controller/Mensaje.java
@@ -0,0 +1,23 @@
+
+package com.personasdeploy.youtube.Controller;
+
+
+public class Mensaje {
+    
+    public String mensaje;
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
